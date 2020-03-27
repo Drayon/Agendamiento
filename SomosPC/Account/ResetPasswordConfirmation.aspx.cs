f9d439982c0b86a3cc9636335c5c8691a97e1be8
Andres <Andres@ANDRES>
2020-03-26 22:33:43 -0300

    Agregar archivos de proyecto.

---
+++ SomosPC/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SomosPC.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SomosPC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SomosPC.Global" Language="C#" %>
